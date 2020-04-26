98be7a06c6d96edcbc9f714b5df9eb004186e131
ALIENQuake <alienquake@hotmail.com>
2020-04-26 20:23:54 +0200

    traify ysblksto.d and yselcol2.d

--- FishingForTrouble/dialog/yselcol2.d
+++ FishingForTrouble/dialog/yselcol2.d
@@ -2,7 +2,7 @@ BEGIN yselcol2
 
 //---------------------------------------------------------
 IF ~Global("ys_TalkedToEildaren","GLOBAL",0)~THEN BEGIN 0
-	SAY ~Thank you for saving my life. That was a good thing that you did, going all that way to Imradim.~
+	SAY @6267
 	IF~~THEN EXIT
 END
 
